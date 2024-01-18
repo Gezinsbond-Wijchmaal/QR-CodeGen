6835865815f97008f2ebac1ea4063d6f1e8d126b
Mark Lemmens <info@tweagle.eu>
2024-01-18 15:18:33 +0100

    menu aanpassing aan groter lettertype

--- gbwmenu.py
+++ gbwmenu.py
@@ -20,6 +20,7 @@ green_hex = rgb_to_hex((0, 152, 68))
 # Lettertype en grootte
 font_path = 'fonts/SourceSansPro-Bold.ttf'
 font_size = 16
+fontBig_size = 20
 
 # Controleer of het lettertype bestaat
 try:
@@ -34,6 +35,7 @@ sg.theme('Black')
 # Aangepaste fontinstellingen voor knoppen
 button_font = (font_path, font_size, 'bold')  # Verwijder de tekstkleur
 button_text_color = green_hex  # Voeg de tekstkleur toe
+font = (font_path, fontBig_size, 'bold')  # Verwijder de tekstkleur
 
 button_layout = [
     [sg.Button('Start QR Code Generator', key='START_QR', button_color=('white', button_text_color), font=button_font),
@@ -41,15 +43,18 @@ button_layout = [
 ]
 
 layout = [
-    [sg.Text('Welkom bij de Gezinsbond Wijchmaal Tool Selecteerder', justification='center', font=('Helvetica', 16, 'bold'), text_color=button_text_color, pad=(0,10))],  # Verhoog de verticale padding
+    [sg.Text('Welkom bij de Gezinsbond Wijchmaal Tool Selecteerder', justification='center', font=font, text_color=button_text_color, pad=(0,10))],
     [sg.Button('Start QR Code Generator', key='START_QR', button_color=('white', button_text_color), font=button_font),
      sg.Stretch(),
      sg.Button('Start Countdown Timer', key='START_TIMER', button_color=('white', button_text_color), font=button_font)],
     [sg.Stretch(), sg.Button('Sluiten', key='CLOSE', button_color=('white', button_text_color), font=button_font), sg.Stretch()]
 ]
 
+gewenste_breedte = 780
+gewenste_hoogte = 200
+
 # Maak het Window
-window = sg.Window('Gezinsbond Wijchmaal Selecteerder', layout, size=(600, 200))
+window = sg.Window('Gezinsbond Wijchmaal Selecteerder', layout, size=(gewenste_breedte, gewenste_hoogte), grab_anywhere=True, finalize=True)
 
 # Event Loop
 while True:
